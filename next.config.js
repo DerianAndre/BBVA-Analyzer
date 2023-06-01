007a43b0069f57a12ebf7376bfee21098f4974ea
André Castillo <hola@derianandre.com>
2023-05-31 22:27:33 -0600

    chore: update next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  output: "export",
+  trailingSlash: true,
+  distDir: "dist",
+  assetPrefix: "./",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
